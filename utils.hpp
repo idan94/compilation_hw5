4372949d39b0a2ea41f5ff836af14cdf2008408d
nadavSellam <Nadavonyy@gmail.com>
2020-06-11 19:05:25 +0300

    compiles

--- utils.hpp
+++ utils.hpp
@@ -1,5 +1,9 @@
 #include "bp.hpp"
 #include <string>
+#include <sstream>
+#include <iostream>
+
+using namespace std;
 
 // Utils Methods:
 namespace utils_hw5
@@ -11,20 +15,20 @@ namespace utils_hw5
     }
     string make_var(int reg_number)
     {
-        return "%var" + to_string(register_number);
+        return "%var" + to_string(reg_number);
     }
     void assign_number_to_register(int reg_number, int number_to_assign)
     {
-        streamstring to_emit;
-        to_emit << makr_var(reg_number) << "="
+        stringstream to_emit;
+        to_emit << make_var(reg_number) << "="
                 << "add i32 0, " << number_to_assign;
         // CodeBuffer::instance().emit(to_emit);
         // %var5 = add i32 0,
     }
-    void register_assign(int reg_number, int reg_number_a, const &string op, int reg_number_b)
+    void register_assign(int reg_number, int reg_number_a, const string& op, int reg_number_b)
     {
-        streamstring to_emit;
-        to_emit << makr_var(reg_number) << "=" << reg_number_a << op << reg_number_b;
+        stringstream to_emit;
+        to_emit << make_var(reg_number) << "=" << reg_number_a << op << reg_number_b;
         // CodeBuffer::instance().emit(to_emit);
     }
 }
